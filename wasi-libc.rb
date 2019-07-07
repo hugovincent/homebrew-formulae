02d16d55d7221a60705ec3f4e0561267e0985363
Hugo Vincent <hugo.vincent@gmail.com>
2019-07-07 11:51:02 +0100

    peg wasi-libc revision

--- wasi-libc.rb
+++ wasi-libc.rb
@@ -1,7 +1,7 @@
 class WasiLibc < Formula
   desc "WASI libc implementation for WebAssembly"
   homepage "https://wasi.dev/"
-  head "https://github.com/CraneStation/wasi-libc.git"
+  head "https://github.com/CraneStation/wasi-libc.git", :revision => "94d34a13ad223930dce354ac33d3dfc73e9b5122"
   depends_on "llvm" => :build
   keg_only "WASI-only headers clash with system headers"
 
